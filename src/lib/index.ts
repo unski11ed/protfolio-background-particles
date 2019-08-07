f6933b64825b0e0668cb596edc71a7c863776c34
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-08-07 19:03:41 +0200

    Final config

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import EventEmitter from "eventemitter3";
-import * as FpsThrottler from 'fps-throttler';
+import FpsThrottler from 'fps-throttler';
 
 import { Screen } from './screen';
 import { ScreenSize } from './screenSize';
